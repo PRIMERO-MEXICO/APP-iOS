0fccd51c0e0245a6aa4d1bcba7b5530175356e8c
Fernando Arana <feraranas@gmail.com>
2023-02-04 19:09:17 -0600

    Navegación entre Login y Main lista.

--- Login/Views/Main.swift
+++ Login/Views/Main.swift
@@ -9,43 +9,73 @@ import SwiftUI
 
 // View para Main
 struct Main: View {
+    
+    // guarda el nombre de usuario a lo largo de su sesión
+    @State var user: String
+    
+    // guarda el estado de sesión T/F
+    @State var isLogin: Bool
+    
     var body: some View {
-        ZStack(alignment: .center) {
-            Fondo()
-            VStack {
-                HStack {
-                    Spacer()
-                    Image("LogoPM")
-                        .resizable()
-                        .frame(width: 150, height: 150)
-                        .padding(.horizontal, -10)
-                    VStack {
-                        Image("memoji")
+        if isLogin {
+            ZStack(alignment: .center) {
+                Fondo()
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image("LogoPM")
                             .resizable()
-                            .frame(width: 60, height: 60)
-                            .clipShape(Circle())
-                            .overlay {
-                                Circle().stroke(.white, lineWidth: 2)
+                            .frame(width: 150, height: 150)
+                            .padding(.horizontal, -10)
+                        VStack {
+                            Image("memoji")
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                                .clipShape(Circle())
+                                .overlay {
+                                    Circle().stroke(.white, lineWidth: 2)
+                                }
+                                .shadow(radius: 5)
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 60, height: 20)
+                                    .cornerRadius(50)
+                                    .foregroundColor(.black)
+                                    .opacity(0.245)
+                                
+                                Text(self.user)
                             }
-                            .shadow(radius: 5)
-                        Rectangle()
-                            .frame(width: 60, height: 20)
-                            .cornerRadius(50)
-                            .foregroundColor(.black)
-                            .opacity(0.245)
+                        }
+                        .padding(.horizontal, 30)
                     }
-                    .padding(.horizontal, 30)
+                    .padding(.top, 80)
+                    
+                    
+                    Spacer()
+                    
+                    // Botón para cerrar sesión.
+                    Button(action: {
+                        self.isLogin.toggle()
+                    }){
+                        Text("Cerrar Sesión")
+                            .foregroundColor(.white)
+                            .frame(width: UIScreen.main.bounds.width - 30)
+                    }
+                    .frame(width: 250, height: 40, alignment: .center)
+                    .background(Color.blue.opacity(0.9), in: RoundedRectangle(cornerRadius: 6))
+                    .padding(.bottom, 30)
                 }
-                .padding(.top, 80)
-                Spacer()
             }
-            
+        } else {
+            AuthContentView()
         }
     }
 }
 
 struct Main_Previews: PreviewProvider {
     static var previews: some View {
-        Main()
+        let u: String = "nombre"
+        let l: Bool = true
+        Main(user: u, isLogin: l)
     }
 }
