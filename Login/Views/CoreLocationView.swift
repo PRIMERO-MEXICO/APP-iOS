d9f007b061bbfdf08096e1763ee49adae9412c93
Fernando Arana <feraranas@gmail.com>
2023-02-05 14:15:37 -0600

    Tratando de agregar paradas. Ubicación Automatizada Lista

--- Login/Views/CoreLocationView.swift
+++ Login/Views/CoreLocationView.swift
@@ -10,17 +10,21 @@ import SwiftUI
 struct CoreLocationView: View {
     
     @StateObject var locationDataManager = LocationDataManager()
+    @State private var paradaUno: String = ""
+    @State private var paradaDos: String = ""
     
     var body: some View {
         VStack {
+            
+            // UBICACIÓN INICIAL DEL USUARIO
             switch locationDataManager.locationManager.authorizationStatus {
             case .authorizedWhenInUse:
                 Text("Your current location is:")
-                Text("\(locationDataManager.CampoDeTextoOrigen)")
-                
                 // cuando esto esté sucediendo queremos guardar en FireBase las coordenadas: latitude & longitude
+                Text("\(locationDataManager.CampoDeTextoOrigen!)")
                 
                 Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
+                
                 Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
     
             case .restricted, .denied:
@@ -31,12 +35,32 @@ struct CoreLocationView: View {
             default:
                 ProgressView()
             }
+            
+            // PARADA 1 y PARADA 2
+            VStack {
+                TextField(
+                    "Primer parada",
+                    text: $paradaUno
+                )
+                .disableAutocorrection(true)
+                .padding(.bottom, 10)
+                TextField(
+                    "Segunda parada",
+                    text: $paradaDos
+                )
+                .disableAutocorrection(true)
+            }
+            .textFieldStyle(.roundedBorder)
+            .padding(.horizontal, 50)
+            .padding(.top, 25)
+            
         }
     }
 }
 
 struct CoreLocationView_Previews: PreviewProvider {
     static var previews: some View {
+        
         CoreLocationView()
     }
 }
